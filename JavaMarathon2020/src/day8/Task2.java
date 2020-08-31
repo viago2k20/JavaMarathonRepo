2f2fd4a2624b94c81c5a9f9136c0d789b682b7dd
viago2k20 <denzh@mail.ru>
2020-08-31 13:26:55 +0300

    add day8 Task1 and Task2

--- JavaMarathon2020/src/day8/Task2.java
+++ JavaMarathon2020/src/day8/Task2.java
@@ -13,6 +13,6 @@ public class Task2 {
     public static void main(String[] args) {
         Plane airplane = new Plane("Boeing", 2000, 150, 10000);
         System.out.println(airplane);
-        
+
     }
 }
