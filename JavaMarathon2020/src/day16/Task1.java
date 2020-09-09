fe7a505be97cfb3690e447825aa34ca9e9326f38
viago2k20 <denzh@mail.ru>
2020-09-09 17:25:01 +0300

    add some changes to Task1 day16

--- JavaMarathon2020/src/day16/Task1.java
+++ JavaMarathon2020/src/day16/Task1.java
@@ -2,7 +2,6 @@ package day16;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.util.Scanner;
 
 public class Task1 {
@@ -14,12 +13,7 @@ public class Task1 {
             Scanner scan = new Scanner(file);
             stLine = scan.nextLine();
             number = stLine.split(" ");
-//            if (number.length != 10)
-//                try {
-//                    throw new IOException();
-//                } catch (IOException e) {
-//                    System.out.println("Неккоректный входной файл");
-//                }
+
             scan.close();
         } catch (FileNotFoundException e) {
             System.out.println("Файл не найден!");
@@ -31,6 +25,5 @@ public class Task1 {
         System.out.format("Среднее арифметическое чисел : %.15f  -->  %.3f  ", sum / number.length, sum / number.length);
 
     }
-
 }
 
