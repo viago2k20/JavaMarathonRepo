bb558cf58eaff70ef9b1a6c8a21d2dd35089f73d
viago2k20 <denzh@mail.ru>
2020-09-10 07:51:27 +0300

    add some changes to ChessBoard

--- JavaMarathon2020/src/day17/ChessBoard.java
+++ JavaMarathon2020/src/day17/ChessBoard.java
@@ -8,8 +8,8 @@ public class ChessBoard {
     }
 
     public void print() {
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[i].length; j++) {
                 System.out.print(chess[i][j]);
             }
             System.out.println("");
